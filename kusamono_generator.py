0990b09ca0923f03aa8d3a024de4981a59ec23b4
Brendan Coates <brendan.emerson.coates@gmail.com>
2023-02-07 20:13:30 -0800

    brainstorm 1

--- kusamono_generator.py
+++ kusamono_generator.py
@@ -10,6 +10,18 @@ import calscape_airtable.calscape_airtable as calscape_airtable
 def main():
     '''
     do the thing
+
+    how many plants
+    size of plants (height)
+    size of vessel (height and width)
+
+    plant considerations:
+    -blooming season (1 for every season)
+    -leaf type (only 1 per type of leaf, or all same type)
+    -flower color (options for which colors/ how many colors)
+    -fruiting shape/ time
+    -stem/ habit (woody stems vs ferns vs green stems)
+
     '''
     print('starting...')
 
