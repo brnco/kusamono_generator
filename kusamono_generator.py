faac4305b1205b42f946efb5326f302a1018d47c
Brendan Coates <brendan.emerson.coates@gmail.com>
2023-02-07 20:07:59 -0800

    updates

---
+++ kusamono_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+'''
+generates kusamono ideas
+usingplants native to the
+California Floristic Province
+'''
+
+import calscape_airtable.calscape_airtable as calscape_airtable
+
+def main():
+    '''
+    do the thing
+    '''
+    print('starting...')
+
+if __name__ == "__main__":
+    main()
